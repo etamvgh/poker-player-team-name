b09694e2ae1d8005a97d17ac20eb902a1e674db4
ezkisar <arpad.a.kiss@ericsson.com>
2018-09-21 11:39:00 +0200

    fold on round 0 allin

--- player.py
+++ player.py
@@ -4,8 +4,12 @@ class Player:
     VERSION = "Best Python player"
 
     def betRequest(self, game_state):
-        min_to_call = game_state['current_buy_in'] - game_state['players'][game_state['in_action']]['bet']
-        my_money = game_state['players'][game_state['in_action']]['stack']
+        myself = game_state['players'][game_state['in_action']]
+        if ((game_state['round'] == 0) and (game_state['current_buy_in'] >= myself['stack'])):
+            return 0
+
+        min_to_call = game_state['current_buy_in'] - myself['bet']
+        my_money = myself['stack']
         if my_money < min_to_call:
             return my_money
         return random.randint(min_to_call, my_money)
