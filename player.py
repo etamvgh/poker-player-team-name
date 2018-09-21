e97b2fd82f3639655ed1e5fcb70a6f50814061b4
Tamás Végh <tamas.vegh@ericsson.com>
2018-09-21 10:39:17 +0200

    test1

--- player.py
+++ player.py
@@ -4,7 +4,11 @@ class Player:
     VERSION = "Best Python player"
 
     def betRequest(self, game_state):
-        return random.randint(0, game_state['players'][game_state['in_action']]['stack'])
+        min_to_call = game_state['current_buy_in'] - game_state['players'][game_state['in_action']]['bet']
+        my_money = game_state['players'][game_state['in_action']]['stack']
+        if my_money < min_to_call:
+            return my_money
+        return random.randint(min_to_call, my_money)
 
     def showdown(self, game_state):
         pass
